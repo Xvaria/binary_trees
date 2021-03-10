d19162d0c65287a21b3967c352bead1e68be9503
Xvaria <2349@holbertonschool.com>
2021-03-10 08:20:15 +0000

    Ready checks if a binary tree is full

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -8,9 +8,9 @@
 int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (!tree)
-        {
-                return (0);
-        }
+	{
+		return (0);
+	}
 	if (!tree->left && !tree->right)
 	{
 		return (1);
