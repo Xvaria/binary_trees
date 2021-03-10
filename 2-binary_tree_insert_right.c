7a382dc3ff67ab4465c00b5700fcf90f67f00ee6
Xvaria <2349@holbertonschool.com>
2021-03-10 05:55:55 +0000

    Ready inserts a node as the right-child of another node

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -8,23 +8,23 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-        binary_tree_t *nnode;
+	binary_tree_t *nnode;
 
 	if (!parent)
 		return (NULL);
-        nnode = malloc(sizeof(binary_tree_t));
-        if (!nnode)
-                return (NULL);
-        nnode->parent = parent;
-        nnode->left = NULL;
-        nnode->right = NULL;
-        if (parent->right)
-        {
-                nnode->right = parent->right;
-                nnode->right->parent = nnode;
-        }
-        if (nnode->parent)
-                parent->right = nnode;
-        nnode->n = value;
-        return (nnode);
+	nnode = malloc(sizeof(binary_tree_t));
+	if (!nnode)
+		return (NULL);
+	nnode->parent = parent;
+	nnode->left = NULL;
+	nnode->right = NULL;
+	if (parent->right)
+	{
+		nnode->right = parent->right;
+		nnode->right->parent = nnode;
+	}
+	if (nnode->parent)
+		parent->right = nnode;
+	nnode->n = value;
+	return (nnode);
 }
