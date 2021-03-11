de827568f31486169376cbb58cc1f147bd1b10d9
Xvaria <2349@holbertonschool.com>
2021-03-11 05:46:11 +0000

    Correction v2.0 betty style

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -7,42 +7,42 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-        size_t l = 0, r = 0;
+	size_t l = 0, r = 0;
 
-        if (!tree || (!tree->left && !tree->right))
-                return (0);
-        l += binary_tree_height(tree->left) + 1;
-        r += binary_tree_height(tree->right) + 1;
-        if (l > r)
-                return (l);
-        else
-                return (r);
+	if (!tree || (!tree->left && !tree->right))
+		return (0);
+	l += binary_tree_height(tree->left) + 1;
+	r += binary_tree_height(tree->right) + 1;
+	if (l > r)
+		return (l);
+	else
+		return (r);
 }
 
 /**
- * binary_tree_is_full - checks if a binary tree is full
+ * binary_tree_is_perfect - checks if a binary tree is perfect
  * @tree: pointer to the root node of the tree to check
  * Return: 0 or 1
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-        if (!tree)
-        {
-                return (0);
-        }
+	if (!tree)
+	{
+		return (0);
+	}
 	if (binary_tree_height(tree->left) != binary_tree_height(tree->right))
 	{
 		return (0);
-        }
+	}
 	if (!tree->left && !tree->right)
-        {
-                return (1);
-        }
-        if ((tree->left && tree->right) &&
+	{
+		return (1);
+	}
+	if ((tree->left && tree->right) &&
 	    (tree->left->parent == tree->right->parent))
-        {
-                return (binary_tree_is_perfect(tree->left)
-                        * binary_tree_is_perfect(tree->right));
-        }
-        return (0);
+	{
+		return (binary_tree_is_perfect(tree->left)
+			* binary_tree_is_perfect(tree->right));
+	}
+	return (0);
 }
