edc49f99bb51a00e46c651488d421cabecfec2e1
Xvaria <2349@holbertonschool.com>
2021-03-11 05:09:34 +0000

    Correction v2.0 delet conditions and change return

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -7,15 +7,8 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	size_t n = 0;
-
-	if (!tree)
+	if (!tree || (!tree->left && !tree->right))
 		return (0);
-	if (tree->left)
-		n += binary_tree_nodes(tree->left) + 1;
-	if (tree->right)
-		n += binary_tree_nodes(tree->right) + 1;
-	if (!tree->parent)
-		n -= 1;
-	return (n);
+	return (binary_tree_nodes(tree->left)
+		+ binary_tree_nodes(tree->right) + 1);
 }
